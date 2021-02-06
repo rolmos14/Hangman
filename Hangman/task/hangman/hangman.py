159849214f56e096ae970306894f421c26e70588
rolmos14 <olmos.rafa@gmail.com>
2021-02-06 18:13:56 +0100

    New stage of Hangman game

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -8,7 +8,7 @@ word_to_show = "-" * len(word_to_guess)
 
 print("H A N G M A N")
 
-for _ in range(attempts):
+while attempts:
     print("\n" + word_to_show)
     user_letter = input("Input a letter: ")
     if user_letter in word_to_guess:
@@ -16,10 +16,20 @@ for _ in range(attempts):
             for i in range(len(word_to_guess)):
                 if word_to_guess[i] == user_letter:
                     word_to_show = word_to_show[:i] + user_letter + word_to_show[i + 1:]
+            if word_to_guess == word_to_show:
+                break  # while
+        else:
+            print("No improvements")
+            attempts -= 1
     else:
         print("That letter doesn't appear in the word")
+        attempts -= 1
 
-print("",
-      "Thanks for playing",
-      "We'll see how well you did in the next stage",
-      sep="\n")
+if attempts:
+    print("\n",
+          word_to_guess,
+          "You guessed the word!",
+          "You survived!",
+          sep="\n")
+else:
+    print("You lost!")
