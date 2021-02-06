3f7f626acff8fe4b8e54927cb783192d41802b42
rolmos14 <olmos.rafa@gmail.com>
2021-02-06 15:22:22 +0100

    Additional exercises solved

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -2,12 +2,24 @@ import random
 
 
 prog_languages = ["python", "java", "kotlin", "javascript"]
+attempts = 8
 word_to_guess = random.choice(prog_languages)
-word_to_show = word_to_guess[0:3] + (len(word_to_guess) - 3) * "-"
+word_to_show = "-" * len(word_to_guess)
 
-user_word = input(f'''H A N G M A N \nGuess the word {word_to_show}: ''')
+print("H A N G M A N")
 
-if user_word == word_to_guess:
-    print("You survived!")
-else:
-    print("You lost!")
+for _ in range(attempts):
+    print("\n" + word_to_show)
+    user_letter = input("Input a letter: ")
+    if user_letter in word_to_guess:
+        if user_letter not in word_to_show:
+            for i in range(len(word_to_guess)):
+                if word_to_guess[i] == user_letter:
+                    word_to_show = word_to_show[:i] + user_letter + word_to_show[i + 1:]
+    else:
+        print("That letter doesn't appear in the word")
+
+print("",
+      "Thanks for playing",
+      "We'll see how well you did in the next stage",
+      sep="\n")
