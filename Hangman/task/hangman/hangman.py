f52b90bcdf287e9d524fab23d0a126cf5765977e
rolmos14 <olmos.rafa@gmail.com>
2021-02-13 10:38:35 +0100

    New version

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -5,22 +5,34 @@ prog_languages = ["python", "java", "kotlin", "javascript"]
 attempts = 8
 word_to_guess = random.choice(prog_languages)
 word_to_show = "-" * len(word_to_guess)
+letters_guessed = set()
 
 print("H A N G M A N")
 
 while attempts:
+
     print("\n" + word_to_show)
     user_letter = input("Input a letter: ")
+
+    if len(user_letter) != 1:
+        print("You should input a single letter")
+        continue  # while
+
+    if not user_letter.islower():
+        print("Please enter a lowercase English letter")
+        continue  # while
+
+    if user_letter in letters_guessed:
+        print("You've already guessed this letter")
+        continue  # while
+    letters_guessed.add(user_letter)
+
     if user_letter in word_to_guess:
-        if user_letter not in word_to_show:
-            for i in range(len(word_to_guess)):
-                if word_to_guess[i] == user_letter:
-                    word_to_show = word_to_show[:i] + user_letter + word_to_show[i + 1:]
-            if word_to_guess == word_to_show:
-                break  # while
-        else:
-            print("No improvements")
-            attempts -= 1
+        for i in range(len(word_to_guess)):
+            if word_to_guess[i] == user_letter:
+                word_to_show = word_to_show[:i] + user_letter + word_to_show[i + 1:]
+        if word_to_guess == word_to_show:
+            break  # exit while
     else:
         print("That letter doesn't appear in the word")
         attempts -= 1
